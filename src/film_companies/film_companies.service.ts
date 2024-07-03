b6cc61a20b4da6fe223e21983b5f57fc266a3e96
Hiwa sadraden <hiwasedradin@gmail.com>
2024-07-03 21:51:14 +0300

    update film companies service

--- src/film_companies/film_companies.service.ts
+++ src/film_companies/film_companies.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { FilmCompaniesRepository } from './film-companies.repository';
 import { CreateFilmCompaniesDto } from 'src/dto/create-film-companies.dto';
-import { UpdateFilmCompaniesDto } from 'src/dto/update-film-companies.dto';
+import { UpdateFilmCompanyDto } from 'src/dto/update-film-companies.dto';
 
 @Injectable()
 export class FilmCompaniesService {
@@ -11,6 +11,19 @@ export class FilmCompaniesService {
     return this.filmCompaniesRepository.findOne(id);
   }
 
+  async CreateFilm(createFilmCompaniesDto: CreateFilmCompaniesDto) {
+    return this.filmCompaniesRepository.create(createFilmCompaniesDto);
+  }
+
+
+  async UpdateFilm(id: number, updateFilmCompaniesDto: UpdateFilmCompanyDto) {
+    return this.filmCompaniesRepository.update(id, updateFilmCompaniesDto);
+  }
+
+
+  async DeleteFilm(id: number) {
+    return this.filmCompaniesRepository.delete(id);
+  }
   // async create(createFilmCompaniesDto: CreateFilmCompaniesDto) {
   //   return this.filmCompaniesRepository.create(createFilmCompaniesDto);
   // }
@@ -22,9 +35,16 @@ export class FilmCompaniesService {
   // async remove(id: number) {
   //   return this.filmCompaniesRepository.remove(id);
   // }
-  async findByFilmId(filmId: number) {
-    return this.filmCompaniesRepository.findByFilmId(filmId);
+  async findByFilmId(film_id: number) {
+    return this.filmCompaniesRepository.findByFilmId(film_id);
   }
+
+
+  async getFilmCompanies() {
+    return this.filmCompaniesRepository.getFilmCompanies();
+  }
+
+
 
 
 }
