7b129ed7b02f1a545d4bb5bc17ec28e1934f4b3c
Hiwa sadraden <hiwasedradin@gmail.com>
2024-07-03 21:49:57 +0300

    Fixed company routes and API

--- src/dto/create-film-companies.dto.ts
+++ src/dto/create-film-companies.dto.ts
@@ -1,9 +1,12 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsInt, IsNumber, IsString } from "class-validator";
 
 
 export class CreateFilmCompaniesDto {
  @IsString()
  role: string;
+
+ @IsInt()
+  film_id: number;
 
  @IsNumber()
  company_id: number;
