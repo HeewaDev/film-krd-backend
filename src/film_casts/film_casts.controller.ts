84d89dff0253b5e1ce58cd042454944d977d26ce
Hiwa sadraden <hiwasedradin@gmail.com>
2024-07-04 15:24:21 +0300

    refactor: Update FilmCastsController to use Parse

--- src/film_casts/film_casts.controller.ts
+++ src/film_casts/film_casts.controller.ts
@@ -1,35 +1,35 @@
 // film-casts.controller.ts
 
 import { Controller, Get, Post, Put, Delete, Body, Param, Query, ParseIntPipe } from '@nestjs/common';
-import { FilmCastsService } from './film_casts.service'; 
-import { CreateFilmCastDto } from 'src/dto/create-film-casts.dto'; 
-import { UpdateFilmCastDto } from 'src/dto/update-film-casts.dto'; 
+import { FilmCastsService } from './film_casts.service';
+import { CreateFilmCastDto } from 'src/dto/create-film-casts.dto';
+import { UpdateFilmCastDto } from 'src/dto/update-film-casts.dto';
 
 @Controller('film_casts')
 export class FilmCastsController {
-  constructor(private readonly filmCastsService: FilmCastsService) {}
+    constructor(private readonly filmCastsService: FilmCastsService) { }
 
-  
 
-  @Get(':film_id') 
+
+    @Get(':film_id')
     async findByFilmId(@Param('film_id', ParseIntPipe) filmId: string) {
 
         return this.filmCastsService.findByFilmId(+filmId);
     }
-  @Post()
-  async create(@Body() createFilmCastDto: CreateFilmCastDto) {
-    return this.filmCastsService.create(createFilmCastDto);
-  }
+    @Post()
+    async create(@Body() createFilmCastDto: CreateFilmCastDto) {
+        return this.filmCastsService.create(createFilmCastDto);
+    }
 
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() updateFilmCastDto: UpdateFilmCastDto) {
-    return this.filmCastsService.update(+id, updateFilmCastDto);
-  }
+    @Put(':id')
+    async update(@Param('id', ParseIntPipe) film_id: number, @Param('id', ParseIntPipe) cast_id: number, @Body() updateFilmCastDto: UpdateFilmCastDto) {
+        return this.filmCastsService.update(film_id, cast_id, updateFilmCastDto);
+    }
 
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return this.filmCastsService.remove(+id);
-  }
+    @Delete(':id')
+    async remove(@Param('id') film_id: number, @Param('cast_id') cast_id: number) {
+        return this.filmCastsService.remove(film_id, cast_id);
+    }
 
     @Get()
     async findAll() {
