a9471ee1081b32decc5b54fcb42049ffefef5b24
Hiwa sadraden <hiwasedradin@gmail.com>
2024-07-04 13:31:31 +0300

    feat: Add methods for managing film crew in FilmCrewService

--- src/film_crew/film_crew.service.ts
+++ src/film_crew/film_crew.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CrewRepository } from './film_crew.repository';
+import { CreateFilmCrewDto } from 'src/dto/create-filmCrew.dto';
+
 
 @Injectable()
 export class FilmCrewService {
@@ -13,4 +15,22 @@ export class FilmCrewService {
     async findAllCrew(){
         return await this.filmCrewRepository.findAllCrew()
     }
+
+
+    async addCrewToFilm(film_id: number, crew_id: number){
+        return await this.filmCrewRepository.addCrewToFilm(film_id, crew_id)
+    }
+
+
+
+    async UpdateCrewInFilm(film_id: number, crew_id: number, new_crew_id: number){
+        return await this.filmCrewRepository.updateCrewInFilm(film_id, crew_id, new_crew_id)
+    }
+
+
+    async removeCrewFromFilm(film_id: number, crew_id: number){
+        return await this.filmCrewRepository.removeCrewFromFilm(film_id, crew_id)
+    }
+
+    
 }
