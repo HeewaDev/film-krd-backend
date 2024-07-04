d597d2b23a959dfaa3f814c56339e8ff6359662a
Hiwa sadraden <hiwasedradin@gmail.com>
2024-07-04 12:48:38 +0300

    refactor: Remove FilmLocationModule and related files

--- src/film_location/film_location.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FilmLocationController } from './FilmLocationController';
-import { FilmLocationService } from './film_location.service';
-
-@Module({
-  controllers: [FilmLocationController],
-  providers: [FilmLocationService]
-})
-export class FilmLocationModule {}
